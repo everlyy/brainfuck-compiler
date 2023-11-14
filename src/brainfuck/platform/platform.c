9504cd507b33e34b4c4d286b58abead488d74ff9
Everly <66821749+everlyy@users.noreply.github.com>
2023-11-14 22:13:56 +0100

    Add string names for platforms
    And some helper functions for command line parsing

--- src/brainfuck/platform/platform.c
+++ src/brainfuck/platform/platform.c
@@ -1,4 +1,5 @@
 #include "platform.h"
+#include <string.h>
 
 void linux_elf_x86_64_compile(Executable* code, Command* commands, int ncommands);
 void linux_asm_arm64_compile(Executable* code, Command* commands, int ncommands);
@@ -8,6 +9,24 @@ static PlatformCompiler compilers[] = {
     [LINUX_ASM_ARM64]  = { .compile = linux_asm_arm64_compile },
 };
 
+static const char* platform_string_identifiers[] = {
+    [LINUX_ELF_X86_64] = "linux-elf-x86-64",
+    [LINUX_ASM_ARM64]  = "linux-asm-arm64"
+};
+
 PlatformCompiler platform_get_compiler(Platform p) {
     return compilers[p];
+}
+
+const char* const* platform_get_string_identifiers() {
+    return platform_string_identifiers;
+}
+
+int platform_string_identifier_to_platform(const char* str) {
+    for(size_t i = 0; i < PLATFORMS_COUNT; i++) {
+        if(strcmp(str, platform_string_identifiers[i]) == 0)
+            return i;
+    }
+
+    return -1;
 }
--- src/brainfuck/platform/platform.h
+++ src/brainfuck/platform/platform.h
@@ -7,8 +7,9 @@
 typedef void(*compile_func)(Executable* code, Command* commands, int ncommands);
 
 typedef enum {
-    LINUX_ELF_X86_64,
+    LINUX_ELF_X86_64 = 0,
     LINUX_ASM_ARM64,
+    PLATFORMS_COUNT
 } Platform;
 
 typedef struct {
@@ -16,5 +17,7 @@ typedef struct {
 } PlatformCompiler;
 
 PlatformCompiler platform_get_compiler(Platform p);
+const char* const* platform_get_string_identifiers(void);
+int platform_string_identifier_to_platform(const char* str);
 
 #endif // _BRAINFUCK_PLATFORM_H_
