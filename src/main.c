e7dc0c8c8bd9046651a990baeb16152bb8a5c422
Everly <66821749+everlyy@users.noreply.github.com>
2023-11-14 22:14:27 +0100

    Accept platform as command-line argument

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include "brainfuck/brainfuck.h"
 #include "brainfuck/error.h"
+#include "platform/platform.h"
 #include <sys/stat.h>
 
 FILE* open_file_or_exit(const char* filename, const char* mode) {
@@ -20,16 +21,31 @@ static int get_filesize(FILE* file) {
 }
 
 int main(int argc, char** argv) {
+    const char* const* platform_string_identifiers = platform_get_string_identifiers();
+
     char* output_filename = "a.out";
+    const char* platform_str = platform_string_identifiers[0];
 
     if(argc < 2) {
         fprintf(stderr, "ERROR: Wrong arguments\n");
-        printf("USAGE: %s <input file> [output file]\n", argv[0]);
+        printf("USAGE: %s <input file> [output file] [platform]\n", argv[0]);
         return 1;
     }
 
     if(argc >= 3)
         output_filename = argv[2];
+
+    if(argc >= 4)
+        platform_str = argv[3];
+
+    int platform = platform_string_identifier_to_platform(platform_str);
+    if(platform < 0) {
+        fprintf(stderr, "Unknown platform: '%s'\n", platform_str);
+        printf("Available platforms:\n");
+        for(size_t i = 0; i < PLATFORMS_COUNT; i++)
+            printf("  %s\n", platform_string_identifiers[i]);
+        return 1;
+    }
 
     FILE* finput = open_file_or_exit(argv[1], "r");
     FILE* foutput = open_file_or_exit(output_filename, "wb");
@@ -44,7 +60,7 @@ int main(int argc, char** argv) {
     int ncommands = 0;
     Command* commands = bf_parse(input, input_size, &ncommands);
 
-    Executable executable = bf_compile(LINUX_ELF_X86_64, commands, ncommands);
+    Executable executable = bf_compile(platform, commands, ncommands);
     fwrite(executable.data, 1, executable.length, foutput);
     fclose(foutput);
 
