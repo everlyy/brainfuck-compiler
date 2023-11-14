57080ad6b4a3dbe5f9e19cb5b0996fdc51bf88cf
Everly <66821749+everlyy@users.noreply.github.com>
2023-11-14 23:38:30 +0100

    Add start of ARM64 ASM platform
    I've never messed with ARM before so this is all just what I could
    find from docs and various examples.
    No jump instructions yet. I tried to add them but it got stuck in
    a loop and I couldn't figure out how to fix it, I'll do that later

--- src/brainfuck/platform/linux_asm_arm64.c
+++ src/brainfuck/platform/linux_asm_arm64.c
@@ -1,6 +1,79 @@
+#include "command.h"
 #include "platform.h"
 #include "error.h"
+#include <stdio.h>
 
-void linux_asm_arm64_compile(Executable*, Command*, int) {
-    ASSERT(!"Not implemented");
+#define emit(str) e_emit_cstr(str"\n")
+
+void linux_asm_arm64_compile(Executable* exec, Command* commands, int ncommands) {
+    e_set_current(exec);
+
+    emit(".bss");
+    emit("cells_buffer: .skip 32768");
+    emit();
+    emit(".text");
+    emit(".global _start");
+    emit("_start:");
+
+    for(int i = 0; i < ncommands; i++) {
+        Command current = commands[i];
+
+        switch(current.type) {
+        case INC_DP:
+            emit("  add x15, x15, 1");
+            break;
+
+        case DEC_DP:
+            emit("  sub x15, x15, 1");
+            break;
+
+        case INC_AT_DP:
+            emit("  ldr x16, [x15]");
+            emit("  add x16, x16, 1");
+            emit("  str x16, [x15]");
+            break;
+
+        case DEC_AT_DP:
+            emit("  ldr x16, [x15]");
+            emit("  sub x16, x16, 1");
+            emit("  str x16, [x15]");
+            break;
+
+        case OUT_AT_DP:
+            emit("  mov w8, 64");
+            emit("  mov x0, 1");
+            emit("  mov x1, x15");
+            emit("  mov x2, 1");
+            emit("  svc 0");
+            break;
+
+        case IN_AT_DP:
+            emit("  mov w8, 63");
+            emit("  mov x0, 0");
+            emit("  mov x1, x15");
+            emit("  mov x2, 1");
+            emit("  svc 0");
+            break;
+
+        case JUMP_ZERO:
+        case JUMP_NOT_ZERO:
+            ERROR("JUMP_ZERO and JUMP_NOT_ZERO are not implemented for ARM64 yet\n");
+            break;
+
+        case SETUP:
+            emit("  ldr x15, cells");
+            break;
+
+        case EXIT:
+            emit("  mov w8, 93");
+            emit("  mov x0, 0");
+            emit("  svc 0");
+            break;
+
+        default:
+            ASSERT(!"Unreachable");
+        }
+    }
+
+    emit("cells: .word cells_buffer");
 }
